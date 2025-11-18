c3d38ca5d06991d84f6cffbe2d6a01524bec1c47
Piyush219 <piyush66642k@gmail.com>
2025-11-18 18:16:44 +0530

    added zod validator and types

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,14 @@
 import ProductList from "@/components/shared/product/product-list";
-import sampleData from "@/db/sample-data";
+import { getLatestProducts } from "@/lib/actions/product.actions";
 
-const HomePAge = () => {
+
+const HomePAge = async () => {
+
+  const latestProducts = await getLatestProducts();
+
   return (
     <>
-      <ProductList data={sampleData.products} title="New Arrivals" limit={4} />
+      <ProductList data={latestProducts} title="New Arrivals" limit={4} />
     </>
   );
 };
---
+++ lib/actions/product.actions.ts
@@ -0,0 +1,17 @@
+'use server';
+import { PrismaClient } from "@/lib/generated/prisma/client";
+import { convertToPlainObject } from "../utils";
+import { LATEST_PRODUCTS_LIMIT } from "../constants";
+
+export async function getLatestProducts() {
+    const prisma = new PrismaClient();
+
+    const data = await prisma.product.findMany({
+        orderBy: {
+            createdAt: 'desc',
+        },
+        take: LATEST_PRODUCTS_LIMIT,
+    });
+
+    return convertToPlainObject(data);
+}
