927c75673282506b83a41edf8fa7ba1fd2aabb4f
Piyush219 <piyush66642k@gmail.com>
2025-11-22 12:59:00 +0530

    added user modals

--- db/seed.ts
+++ db/seed.ts
@@ -7,8 +7,18 @@ dotenv.config();
 async function main() {
     const prisma = new PrismaClient();
     await prisma.product.deleteMany();
+    await prisma.account.deleteMany();
+    await prisma.verificationToken.deleteMany();
+    await prisma.session.deleteMany();
+    await prisma.user.deleteMany();
+
+
     await prisma.product.createMany({
         data: sampleData.products,
+    });
+
+    await prisma.user.createMany({
+        data: sampleData.users,
     });
     console.log("Database has been seeded.");
 }
