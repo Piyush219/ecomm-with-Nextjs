bafa2c31121319e86fc5844681793088de4aa97f
Piyush219 <piyush66642k@gmail.com>
2026-01-07 16:25:26 +0530

    setting up next auth

---
+++ auth.ts
@@ -0,0 +1,63 @@
+import NextAuth from "next-auth";
+import { prisma } from "./db/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import CredentialProvider from "next-auth/providers/credentials";
+import { compareSync } from "bcrypt-ts-edge";
+import type { NextAuthConfig } from "next-auth";
+export const config = {
+  pages: {
+    signIn: "/sign-in",
+    error: "/sign-in",
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialProvider({
+      credentials: {
+        email: { type: "email" },
+        password: { type: "password" },
+      },
+      async authorize(credentials) {
+        if (credentials == null) return null;
+
+        // finding user in database
+
+        const user = await prisma.user.findFirst({
+          where: { email: credentials.email as string },
+        });
+
+        if (user && user.password) {
+          const isMatch = compareSync(
+            credentials.password as string,
+            user.password
+          );
+
+          if (isMatch) {
+            return {
+              id: user.id,
+              email: user.email,
+              role: user.role,
+            };
+          }
+        }
+
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, token, user, trigger } : any) {
+        // setting user id from the token
+        session.user.id = token.sub;
+        if(trigger === "update" ) {
+            session.user.name = user.name;
+        }
+        return session;
+    }
+  }
+} satisfies NextAuthConfig;
+
+export const { handlers, signIn, signOut, auth } = NextAuth(config);
