7194866c36dacdfbb742b6eb1dfbb5d5088d4f91
Piyush219 <piyush66642k@gmail.com>
2026-03-16 18:20:31 +0530

    added orders page and order action

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,63 +1,70 @@
 /** @type {import('tailwindcss').Config} */
 
-import animate from 'tailwindcss-animate'
+import animate from "tailwindcss-animate";
 
 export default {
   theme: {
+    container: {
+      center: true, // This fixes the "mx-auto" issue globally
+      padding: "1rem", // Adds default side padding
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {},
   },
   plugins: [
     animate,
     function ({ addUtilities }) {
       addUtilities({
-        '.wrapper': {
-          'max-width': '80rem', // max-w-7xl
-          'margin-left': 'auto',
-          'margin-right': 'auto',
-          'padding': '1.25rem', // p-5
-          'width': '100%',
-          '@screen md': {
-            'padding-left': '2.5rem', // md:px-10
-            'padding-right': '2.5rem',
+        ".wrapper": {
+          "max-width": "80rem", // max-w-7xl
+          "margin-left": "auto",
+          "margin-right": "auto",
+          padding: "1.25rem", // p-5
+          width: "100%",
+          "@screen md": {
+            "padding-left": "2.5rem", // md:px-10
+            "padding-right": "2.5rem",
           },
         },
-        '.flex-start': {
-          display: 'flex',
-          'justify-content': 'flex-start',
-          'align-items': 'center',
+        ".flex-start": {
+          display: "flex",
+          "justify-content": "flex-start",
+          "align-items": "center",
         },
-        '.flex-center': {
-          display: 'flex',
-          'justify-content': 'center',
-          'align-items': 'center',
+        ".flex-center": {
+          display: "flex",
+          "justify-content": "center",
+          "align-items": "center",
         },
-        '.flex-between': {
-          display: 'flex',
-          'justify-content': 'space-between',
-          'align-items': 'center',
+        ".flex-between": {
+          display: "flex",
+          "justify-content": "space-between",
+          "align-items": "center",
         },
-        '.h1-bold': {
-          'font-weight': '700',
-          'font-size': '1.875rem', // text-3xl
-          '@screen lg': {
-            'font-size': '2.25rem', // lg:text-4xl
+        ".h1-bold": {
+          "font-weight": "700",
+          "font-size": "1.875rem", // text-3xl
+          "@screen lg": {
+            "font-size": "2.25rem", // lg:text-4xl
           },
         },
-        '.h2-bold': {
-          'font-weight': '700',
-          'font-size': '1.5rem', // text-2xl
-          '@screen lg': {
-            'font-size': '1.875rem', // lg:text-3xl
+        ".h2-bold": {
+          "font-weight": "700",
+          "font-size": "1.5rem", // text-2xl
+          "@screen lg": {
+            "font-size": "1.875rem", // lg:text-3xl
           },
         },
-        '.h3-bold': {
-          'font-weight': '700',
-          'font-size': '1.25rem', // text-xl
-          '@screen lg': {
-            'font-size': '1.5rem', // lg:text-2xl
+        ".h3-bold": {
+          "font-weight": "700",
+          "font-size": "1.25rem", // text-xl
+          "@screen lg": {
+            "font-size": "1.5rem", // lg:text-2xl
           },
         },
-      })
+      });
     },
   ],
-}
+};
