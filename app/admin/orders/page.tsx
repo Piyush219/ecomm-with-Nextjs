9c726cd3343e6a7c42e0672e27f8322ccf7f89af
Piyush219 <piyush66642k@gmail.com>
2026-03-25 17:13:25 +0530

    admin auth guard and admin orders page

---
+++ app/admin/orders/page.tsx
@@ -0,0 +1,90 @@
+import { auth } from "@/auth";
+import Pagination from "@/components/shared/pagination";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { getAllOrders } from "@/lib/actions/order.actions";
+import { requireAdmin } from "@/lib/auth-guard";
+import { formatCurrency, formatDateTime, formatId } from "@/lib/utils";
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Admin Orders",
+};
+
+const AdminOrderPage = async (props: {
+  searchParams: Promise<{ page: string }>;
+}) => {
+  const { page } = await props.searchParams;
+
+  await requireAdmin();
+
+  const orders = await getAllOrders({
+    page: Number(page),
+    // limit: 2,
+  });
+
+  console.log(orders);
+
+  return (
+    <div className="space-y-2">
+      <h2 className="h2-bold">Orders</h2>
+      <div className="overflow-x-auto">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>DATE</TableHead>
+              <TableHead>TOTAL</TableHead>
+              <TableHead>PAID</TableHead>
+              <TableHead>DELIVERED</TableHead>
+              <TableHead>ACTION</TableHead>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+            {orders.data.map((order) => (
+              <TableRow key={order.id}>
+                <TableCell>{formatId(order.id)}</TableCell>
+                <TableCell>{formatCurrency(order.totalPrice)}</TableCell>
+                <TableCell>
+                  {formatDateTime(order.createdAt).dateTime}
+                </TableCell>
+                <TableCell>
+                  {order.isPaid && order.paidAt
+                    ? formatDateTime(order.paidAt).dateTime
+                    : " Not Paid"}
+                </TableCell>
+                <TableCell>
+                  {order.isDelivered && order.deliveredAt
+                    ? formatDateTime(order.deliveredAt).dateTime
+                    : "Not Delivered"}
+                </TableCell>
+                <TableCell>
+                  <Button asChild variant="outline" size="sm">
+                    <Link href={`/order/${order.id}`}>Details</Link>
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        {orders.totalPages > 1 && (
+          <Pagination
+            page={Number(page) || 11}
+            totalPages={orders?.totalPages}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AdminOrderPage;
